87c8cb20129764c01ae8e68c6633a524ac4ab8d9
Jay-boo <jeanalainpro2@gmail.com>
2023-01-19 16:59:22 +0100

    crawling using robots.txt

--- TP1/crawl.py
+++ TP1/crawl.py
@@ -3,107 +3,127 @@ import time
 from bs4 import BeautifulSoup
 import urllib.robotparser
 from urllib.parse import urlparse
-import os
 import socket
+from htmldate import find_date
 
-def get_page_url(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    res=soup.find_all('a')
-    return res
-
+from requests.api import get
+from requests.exceptions import TooManyRedirects
 
 socket.setdefaulttimeout(1)
 
 
+def url_date(url):
+    find_date(url)
+
+
+
+def get_allowed_urls(url):
+    pages=[]
+    try:
+        response = requests.get(url)
+    except TooManyRedirects:
+        print("We find a Too manyRedirect error while navigating through urls")
+        return []
+
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    links=soup.find_all('a')
+    
+    for link in links:
+        link=link.get('href')
+        rp=urllib.robotparser.RobotFileParser()
+        try:
+            rp.set_url(urlparse(link).scheme + "://"+ urlparse(link).hostname +"/robots.txt")
+            rp.read()
+            if rp.can_fetch("*",link):
+                pages.append(link)
+        except:
+            pass
+    return list(set(pages))
+
+            
+
+
+
+
+
+
+
 class Crawler:
-    def __init__(self,seed:str,stop:int=1000):
+    def __init__(self,seed:str,stop:int=100):
         self.seed=seed
         self.stop=stop
         self.frontier=[]
         self.output=[]
 
+    def crawl(self):
+        first_allowed_urls=get_allowed_urls(self.seed)
+        print(first_allowed_urls)
+        self.frontier=first_allowed_urls
+        self.output=first_allowed_urls
+        if  len(self.frontier)>=self.stop :
+            return self.output[0:self.stop]
 
-    def filter_allowed_urls(self,urls):
-        """
-        Check robot.tx
-        """
-        output=[]
-        print("In url filter method:")
-        print([url.get('href') for url in urls])
-        for link in urls: 
-            link=link.get('href')
-            rp=urllib.robotparser.RobotFileParser()
-            try:
-                rp.set_url(urlparse(link).scheme + "://"+ urlparse(link).hostname +"/robots.txt")
-                rp.read()
-                if rp.can_fetch("*",link):
-                    output.append(link)
-            except:
-                pass
+        while  True:
+            print("in  while loop")
+            flag=True
+            new_outputs=[]
+            waiting_url_output=self.output
             
-
-        return output
-
-
-
-
-    def run(self):
-        
-        links=get_page_url(self.seed)
-        self.frontier=links
-        self.output=self.filter_allowed_urls(self.frontier)
-        
-        if len(self.output)>= self.stop:
-            return
-        print('------------- outputs')
-        print(self.output)
-        print('---------------------------')
-
-        self.frontier=self.output
-            
-
-        while True: 
-            
-            flag=False
-            new_frontier=[]
-            output_maj=self.output
             for url in self.frontier:
-                print(f'-----------------------------------------crawled :{url}')
-                links=get_page_url(url)
-                new_frontier.append(links)
-                new_output=self.filter_allowed_urls(links)
-                print(new_output)
-                output_maj.remove(url)
-                output_maj.extend(new_output)
-                print('------------------OUTPUT LEN')
-                print(len(output_maj))
+                print(f"----------------------looking at {url} in frontier")
+                #-----------------------
+                new_outputs+=get_allowed_urls(url)
+                print(f"Total size of the outputs: {len(list(set(new_outputs+waiting_url_output)))}")
 
-                if len(output_maj) >=self.stop :
-                    print(url)
-                    flag=True
-                    break
+                #------------------
+                # Test if we have enough outputs :
+                waiting_url_output.remove(url)
+                if len(list(set(new_outputs+waiting_url_output))) >= self.stop:
+                    flag=False
+                    break;
 
-                time.sleep(5)
+                time.sleep(5)#Time page crawling
 
-            print('-----------------------------end of first urls')
-            self.output=output_maj
-            self.frontier=self.output
-                
-            if flag:
-                break
-        print(self.output)
-        print(len(self.output))
-
+            print("----- See every url in the actual output")
+            #------------------------
+            # update attribute
+            final_output=list(set(waiting_url_output+new_outputs))
             
-                    
+            #-----------------
+            # Detect when we have no more url
+            difference=True
+            for x in set(final_output +self.output):
+                if final_output.count(x)!=self.output.count(x):
+                    difference= False
+            if difference:
+                print("no more urls")
+                flag=False
+
+            self.frontier=final_output
+            self.output=final_output
+            
+            if not flag:
+                print("We have enough output or there is no more url ")
+                break
+                
+        return self.output[0:self.stop]
 
 
+    def write_output_urls_in_text_file(self):
+        with open("crawled_webpages.txt","w") as f:
+            for line in self.output[0:self.stop]:
+                f.write(line+"\n")
+
+    def save_in_RDB(self):
+        pass
 
 if __name__ == "__main__":
     # url="https://ensai.fr/"
     url="https://twitter.com/"
-    crawler=Crawler(url,500)
+    crawler=Crawler(url,100)
 
-    crawler.run()
+    crawler.crawl()
+    crawler.write_output_urls_in_text_file()
+
 
